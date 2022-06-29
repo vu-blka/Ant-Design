cb7e91d36dcf40fa868388a9aac0b171bd02393d
Kevin <hiimvyxinh@gmail.com>
2022-06-29 20:07:46 +0700

    5: Using grid

--- tech/src/views/home.js
+++ tech/src/views/home.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import AppHero from "../components/home/hero.js";
+import AppAbout from "../components/home/about.js";
 
 function AppHome() {
   return (
     <div className="main">
       <AppHero />
+      <AppAbout />
     </div>
   );
 }
