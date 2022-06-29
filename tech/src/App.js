cc7fbb41ce0178e6f676c97e5c5c74d694f05cbf
Kevin <hiimvyxinh@gmail.com>
2022-06-30 00:26:40 +0700

    11: Work on footer and back to top component

--- tech/src/App.js
+++ tech/src/App.js
@@ -1,24 +1,27 @@
-import './App.css';
+import "./App.css";
 
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
-import AppHeader from './components/common/header.js'
-import AppHome from './views/home.js'
+import AppHeader from "./components/common/header.js";
+import AppHome from "./views/home.js";
+import AppFooter from "./components/common/footer.js";
 
-
-import { Layout } from 'antd';
-const { Header, Content} = Layout;
+import { Layout } from "antd";
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <Layout className="mainLayout">
-    <Header>
-      <AppHeader />
-    </Header>
-    <Content>
-      <AppHome />
-    </Content>
-  </Layout>
+      <Header>
+        <AppHeader />
+      </Header>
+      <Content>
+        <AppHome />
+      </Content>
+      <Footer>
+        <AppFooter />
+      </Footer>
+    </Layout>
   );
 }
 
