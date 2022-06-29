c47ce48cf04e627408359e43c7dbd68aac6d9c46
Kevin <hiimvyxinh@gmail.com>
2022-06-29 22:23:24 +0700

    8: Work on FAQ using collapse component

--- tech/src/views/home.js
+++ tech/src/views/home.js
@@ -4,6 +4,7 @@ import AppHero from "../components/home/hero.js";
 import AppAbout from "../components/home/about.js";
 import AppCard from "../components/home/fearture.js";
 import AppWork from "../components/home/works.js";
+import AppFAQ from "../components/home/faq.js";
 
 function AppHome() {
   return (
@@ -12,6 +13,7 @@ function AppHome() {
       <AppAbout />
       <AppCard />
       <AppWork />
+      <AppFAQ />
     </div>
   );
 }
